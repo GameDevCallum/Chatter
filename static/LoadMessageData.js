4a33391359ee64d196611f8c0bbe1f94819b3f4c
Callum S <gamedevcallum@gmail.com>
2020-08-06 18:58:51 +0100

    Page checks for new messages and displays

--- static/LoadMessageData.js
+++ static/LoadMessageData.js
@@ -1,22 +1,42 @@
+var current = 0;
+
+
 function LoadMessages()
 {
+      console.log("Hello World!");
       $.getJSON("http://127.0.0.1:8000/static/MessageData.json", function(json) {
             const messageList = json.Messages.reverse();
 
-            for (var i = 0; i < Object.keys(messageList).length; i++)
+            if (json.Messages[0])
             {
-                  var container = document.createElement("div");
-                  document.body.appendChild(container);
+                  if (current != messageList[0].id)
+                  {
+                        for (var i = 0; i < Object.keys(messageList).length; i++)
+                        {
+                              var container = document.createElement("div");
+                              document.body.appendChild(container);
+            
+                              var username = document.createElement("span");
+                              container.appendChild(username);
+                              username.style.color = "gray";
+                              username.textContent = json.Messages[i].username + ": ";
+            
+                              var message = document.createElement("span");
+                              container.appendChild(message);
+                              message.style.color = "black";
+                              message.textContent = json.Messages[i].message;
+                        }
+                        current = messageList[0].id;
+                  }
+            }
+            
 
-                  var username = document.createElement("span");
-                  container.appendChild(username);
-                  username.style.color = "gray";
-                  username.textContent = json.Messages[i].username + ": ";
-
-                  var message = document.createElement("span");
-                  container.appendChild(message);
-                  message.style.color = "black";
-                  message.textContent = json.Messages[i].message;
+            if (current == 0)
+            {
+                  if (json.Messages[0])
+                  {
+                        current = messageList[0].id;
+                  }
             }
       });
 }
